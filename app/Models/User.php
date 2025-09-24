f671ae42db76466935629f378d1432565a8aa1fc
Javier <sharvel.irigoyen@gmail.com>
2025-09-23 20:20:19 -0500

    fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ use Jeffgreco13\FilamentBreezy\Traits\TwoFactorAuthenticatable;
 use Spatie\Activitylog\Traits\CausesActivity;
 use Spatie\Activitylog\Traits\LogsActivity;
 use Filament\Panel;
-
+use Filament\Models\Contracts\FilamentUser;
 
 class User extends Authenticatable implements HasAvatar, FilamentUser
 {
